f78cd449ca4bdf704d03c787ce575e80825f07ba
Kyle Bowerman <kyle.bowerman@gmail.com>
2018-01-08 16:05:29 -0600

    cleaned up cpp

--- hotbox.cpp
+++ hotbox.cpp
@@ -18,7 +18,6 @@ void setup()
     Particle.variable("version", MYVERSION);
   	// To read the values from a browser, go to:
   	// http://api.particle.io/v1/devices/{DEVICE_ID}/{VARIABLE}?access_token={ACCESS_TOKEN}
-
 	lipo.begin(); // Initialize the MAX17043 LiPo fuel gauge
   	lipo.quickStart(); // Quick start restarts the MAX17043 in hopes of getting a more accurate guess for the SOC.
     lipo.setThreshold(20); // Set alert threshold to 20%.
@@ -29,24 +28,10 @@ void loop()
 {
 
 	voltage = lipo.getVoltage();
-
 	soc = lipo.getSOC();
-
 	alert = lipo.getAlert();
 
-	// Those variables will update to the Spark Cloud, but we'll also print them
-	// locally over serial for debugging:
-	Serial.print("Lucky Voltage: ");
-	Serial.print(voltage);  // Print the battery voltage
-	Serial.println(" V");
 
-	Serial.print("Lucky Alert: ");
-	Serial.println(alert);
-
-	Serial.print("Lucky Percentage: ");
-	Serial.print(soc); // Print the battery state of charge
-	Serial.println(" %");
-	Serial.println();
 
 	delay(500);
 }
