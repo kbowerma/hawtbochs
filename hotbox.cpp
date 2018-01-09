c9eca257097945ff6e351f0d1217c3481b460751
Kyle Bowerman <kyle.bowerman@gmail.com>
2018-01-09 11:27:27 -0600

    working temp up to 5 sensors,  devicecount needs reboot but get temp is hot swappable

--- hotbox.cpp
+++ hotbox.cpp
@@ -8,6 +8,7 @@
 #include "lib/OneWire/OneWire.h"
 #include "lib/SparkDallas/spark-dallas-temperature.h"
 #include "lib/SparkFun_MAX17043_Particle_Library/firmware/SparkFunMAX17043.h"
+#include "lib/streaming/firmware/spark-streaming.h"
 #include "hotbox.h"
 
 
@@ -25,12 +26,16 @@ void setup()
   	Particle.variable("file", FILENAME);
     Particle.variable("version", MYVERSION);
 		Particle.variable("devices",deviceCount);
+		Particle.function("q", queryDevices);
   	// To read the values from a browser, go to:
   	// http://api.particle.io/v1/devices/{DEVICE_ID}/{VARIABLE}?access_token={ACCESS_TOKEN}
 	lipo.begin(); // Initialize the MAX17043 LiPo fuel gauge
   	lipo.quickStart(); // Quick start restarts the MAX17043 in hopes of getting a more accurate guess for the SOC.
     lipo.setThreshold(20); // Set alert threshold to 20%.
-	deviceCount =  getDeviceCount(); //Need to set the device Index Array at startup
+
+//Need to set the device Index Array at startup
+  deviceCount = getDeviceCount();
+  queryDevices("auto");
 
 }
 
@@ -46,8 +51,50 @@ void loop()
 	delay(500);
 }
 
+// Functions
+
 int getDeviceCount() {
   //sensor.begin();
   deviceCount = sensor.getDeviceCount();
   return deviceCount;
 }
+
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++)
+  {
+          Serial.print(" ");
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+int queryDevices(String command) {
+
+    if(command == "auto" || command == "") {
+    // sets and prints the device array
+        for(int i=0; i < deviceCount; i++ ) {
+            sensor.getAddress(deviceIndexArray[i], i);
+            Serial.print("Device Index ");
+            Serial.print(i);
+            printAddress(deviceIndexArray[i]);
+            Serial.print("\n");
+        }
+        Serial.print("--------------------------------------\n");
+        return deviceCount;
+    }
+
+		if(command == "t") {
+			sensor.requestTemperatures();
+			for(int i=0; i < 5; i++ ) {
+				deviceTemp[i] = sensor.getTempF(*deviceAddressArray[i]);
+				if ( deviceTemp[i] > 0 ) {
+				  Serial << "Device " << deviceName[i] << " temp " <<  deviceTemp[i] << endl;
+				}	
+
+
+			}
+			Serial.print("--------------------------------------\n");
+			return deviceCount;
+		}
+	}
--- hotbox.h
+++ hotbox.h
@@ -1,6 +1,6 @@
 //defines
 #define FILENAME "hotbox"
-#define MYVERSION "0.2.a"
+#define MYVERSION "0.2.0"
 #define ONE_WIRE_BUS D2
 
 
@@ -13,7 +13,19 @@
 
 //Prototypes
  int getDeviceCount();
+ void printAddress(DeviceAddress deviceAddress);
+ int queryDevices(String command) ;
 
 //Declarations
  OneWire oneWire(ONE_WIRE_BUS);
  DallasTemperature sensor(&oneWire);
+
+DeviceAddress deviceIndexArray[5];  // Dyanamic array,   I think I only use it for the print
+ DeviceAddress ta1 = { 0x28, 0xB6, 0x98, 0x58, 0x06, 0x00, 0x00, 0x37 };
+ DeviceAddress tb1 = { 0x28, 0xED, 0x33, 0xDD, 0x06, 0x00, 0x00, 0xFA };
+ DeviceAddress td1 = { 0x28, 0xC8, 0xA0, 0xDE, 0x06, 0x00, 0x00, 0xD6 };
+ DeviceAddress te1 = { 0x28, 0xFA, 0xD1, 0x3A, 0x06, 0x00, 0x00, 0xB2 };
+ DeviceAddress tc1 = { 0x28, 0xF5, 0x39, 0xDD, 0x06, 0x00, 0x00, 0xBD };
+ DeviceAddress* deviceAddressArray[5] = {  &ta1, &tb1, &tc1, &td1, &te1 };
+ String deviceName[5] = {"a1","b1","c1","td1","e1"};
+ double deviceTemp[5] = {0.0, 0.0, 0.0, 0.0, 0.0}; //initialize device tempartures
